7f7a7541d222a28ef008a93bf678f3dba09ae967
Enrico <enrico.milanese@whoi.edu>
2025-11-21 14:11:46 -0500

    add checks before adding submodules, bash shebangs, fix bug for install_cupid flag in init.sh

--- install/init.sh
+++ install/init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -7,7 +7,11 @@ source ./envpaths.sh
 
 if [[ "$INSTALL_CROCODASH" -eq 1 ]]; then
     echo "Downloading CrocoDash..."
-    git submodule add https://github.com/CROCODILE-CESM/CrocoDash.git "$CROCODASH_PATH"
+    if [ ! -d "$CROCODASH_PATH" ]; then
+        git submodule add https://github.com/CROCODILE-CESM/CrocoDash.git "$CROCODASH_PATH"
+    else
+        echo "CrocoDash already exists at $CROCODASH_PATH, skipping submodule add."
+    fi
     cd "$CROCODASH_PATH"
     git fetch --tags
     git checkout v0.1.0-beta
@@ -21,7 +25,11 @@ fi
 
 if [[ "$INSTALL_CROCOCAMP" -eq 1 ]]; then
     echo "Downloading CrocoCamp..."
-    git submodule add https://github.com/CROCODILE-CESM/CrocoCamp.git "$CROCOCAMP_PATH"
+    if [ ! -d "$CROCOCAMP_PATH" ]; then
+        git submodule add https://github.com/CROCODILE-CESM/CrocoCamp.git "$CROCOCAMP_PATH"
+    else
+        echo "CrocoCamp already exists at $CROCOCAMP_PATH, skipping submodule add."
+    fi
     cd "$CROCOCAMP_PATH"
     git fetch --tags
     git checkout 4b785f3
@@ -31,9 +39,13 @@ fi
 
 #### CUPiD
 
-if [[ "$INSTALL_CROCOCAMP" -eq 1 ]]; then
+if [[ "$INSTALL_CUPID" -eq 1 ]]; then
     echo "Downloading CUPiD..."
-    git submodule add https://github.com/NCAR/CUPiD.git "$CUPID_PATH"
+    if [ ! -d "$CUPID_PATH" ]; then
+        git submodule add https://github.com/NCAR/CUPiD.git "$CUPID_PATH"
+    else
+        echo "CUPiD already exists at $CUPID_PATH, skipping submodule add."
+    fi
     cd "$CUPID_PATH"
     git fetch --tags
     git checkout v0.3.1
@@ -48,7 +60,11 @@ fi
 
 if [[ "$INSTALL_CESM" -eq 1 ]]; then
     echo "Downloading CESM..."
-    git submodule add -b workshop_2025 https://github.com/CROCODILE-CESM/CESM "$CESM_PATH"
+    if [ ! -d "$CESM_PATH" ]; then
+        git submodule add -b workshop_2025 https://github.com/CROCODILE-CESM/CESM "$CESM_PATH"
+    else
+        echo "CESM already exists at $CESM_PATH, skipping submodule add."
+    fi
     cd "$CESM_PATH"
     git pull
     git submodule update --init
