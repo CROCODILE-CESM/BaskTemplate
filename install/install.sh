6c436f7d2e3fbcb8bfbc92c6c90f9c565f2f4067
Enrico <enrico.milanese@whoi.edu>
2026-06-11 16:38:00 -0600

    recap paths and environments once install is finished

--- install/install.sh
+++ install/install.sh
@@ -60,9 +60,9 @@ source ./setup_conda_env.sh
 if [[ "$INSTALL_CROCODASH" -eq 1 ]]; then
     echo "Installing CrocoDash environment..."
     ENV_NAME=$(awk -F ": " '/^name:/ {print $2}' "$CROCODASH_PATH/environment.yml")
-    NEW_ENV_NAME="${ENV_PREFIX}-${ENV_NAME}"
-    mamba env create -f "$CROCODASH_PATH"/environment.yml --name ${NEW_ENV_NAME} --yes
-    add_env_vars_to_conda "$NEW_ENV_NAME"
+    CROCODASH_ENV_NAME="${ENV_PREFIX}-${ENV_NAME}"
+    mamba env create -f "$CROCODASH_PATH"/environment.yml --name ${CROCODASH_ENV_NAME} --yes
+    add_env_vars_to_conda "$CROCODASH_ENV_NAME"
     echo "CrocoDash environment installed."
 fi
 
@@ -71,7 +71,8 @@ if [[ "$INSTALL_MODEL2OBS" -eq 1 ]]; then
     echo "Installing model2obs environment..."
     cd "$MODEL2OBS_PATH"/install
     cp envpaths.sh.template envpaths.sh
-    DART_ROOT_PATH=${DART_PATH} CONDA_ENV_NAME="${ENV_PREFIX}-model2obs" ./install.sh
+    MODEL2OBS_ENV_NAME="${ENV_PREFIX}-model2obs"
+    DART_ROOT_PATH=${DART_PATH} CONDA_ENV_NAME=${MODEL2OBS_ENV_NAME} ./install.sh
     cd "$INSTALL_DIR"
     echo "model2obs environment installed."
 fi
@@ -81,14 +82,14 @@ if [[ "$INSTALL_CUPID" -eq 1 ]]; then
     echo "Installing CUPiD environments..."
 
     ENV_NAME=$(awk -F ": " '/^name:/ {print $2}' "$CUPID_PATH"/environments/cupid-infrastructure.yml)
-    NEW_ENV_NAME="${ENV_PREFIX}-${ENV_NAME}"
-    mamba env create -f "$CUPID_PATH"/environments/cupid-infrastructure.yml --name ${NEW_ENV_NAME} --yes
-    add_env_vars_to_conda "$NEW_ENV_NAME"
+    CUPID_ENV1_NAME="${ENV_PREFIX}-${ENV_NAME}"
+    mamba env create -f "$CUPID_PATH"/environments/cupid-infrastructure.yml --name ${CUPID_ENV1_NAME} --yes
+    add_env_vars_to_conda "$CUPID_ENV1_NAME"
 
     ENV_NAME=$(awk -F ": " '/^name:/ {print $2}' "$CUPID_PATH"/environments/cupid-analysis.yml)
-    NEW_ENV_NAME="${ENV_PREFIX}-${ENV_NAME}"
-    mamba env create -f "$CUPID_PATH"/environments/cupid-analysis.yml --name ${NEW_ENV_NAME} --yes
-    add_env_vars_to_conda "$NEW_ENV_NAME"
+    CUPID_ENV2_NAME="${ENV_PREFIX}-${ENV_NAME}"
+    mamba env create -f "$CUPID_PATH"/environments/cupid-analysis.yml --name ${CUPID_ENV2_NAME} --yes
+    add_env_vars_to_conda "$CUPID_ENV2_NAME"
 
     echo "CUPiD environments installed."
 fi
@@ -100,4 +101,30 @@ if [[ "$INSTALL_CESM" -eq 1 ]]; then
     ./bin/git-fleximod update --path "$CESM_PATH"
     cd "$INSTALL_DIR"
     echo "CESM installed."
+fi
+
+echo ""
+echo "------------------------------------------------------------------"
+echo "Install complete."
+echo "Components, environments and paths installed:"
+
+if [[ "$INSTALL_CROCODASH" -eq 1 ]]; then
+    echo "CrocoDash:"
+    echo "  path: $CROCODASH_PATH"
+    echo "  conda environments: $CROCODASH_ENV_NAME"
+fi
+if [[ "$INSTALL_CESM" -eq 1 ]]; then
+    echo "CESM:"
+    echo "  path: $CESM_PATH"
+fi
+if [[ "$INSTALL_MODEL2OBS" -eq 1 ]]; then
+    echo "MODEL2OBS:"
+    echo "  path: $MODEL2OBS_PATH"
+    echo "  conda environment: $MODEL2OBS_ENV_NAME"
+fi
+if [[ "$INSTALL_CUPID" -eq 1 ]]; then
+    echo "CUPiD:"
+    echo "  path: $CUPID_PATH"
+    echo "  conda environments: $CUPID_ENV1_NAME"
+    echo "                      $CUPID_ENV2_NAME"
 fi
